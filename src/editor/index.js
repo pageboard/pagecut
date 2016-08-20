66869d0db214278ec6e84e6906d20950b0fce043
Jérémy Lal <kapouer@melix.org>
2016-08-20 10:52:50 +0200

    Enable more tags

--- src/editor/index.js
+++ src/editor/index.js
@@ -1,6 +1,7 @@
 const {ProseMirror} = require("prosemirror/dist/edit");
 const {Schema} = require("prosemirror/dist/model");
 const basicSchema = require("prosemirror/dist/schema-basic");
+const tableSchema = require("prosemirror/dist/schema-table");
 const {exampleSetup, buildMenuItems} = require("prosemirror/dist/example-setup");
 const {tooltipMenu, menuBar, selectParentNodeItem} = require("prosemirror/dist/menu");
 
@@ -11,7 +12,21 @@ const BreaksPlugin = require("./breaks-plugin");
 let schemaSpec = {
 	nodes: {
 		doc: {type: basicSchema.Doc, content: "block+"},
+
 		paragraph: { type: basicSchema.Paragraph, content: "inline<_>*", group: "block" },
+		blockquote: { type: basicSchema.BlockQuote, content: "block+", group: "block" },
+		ordered_list: { type: basicSchema.OrderedList, content: "list_item+", group: "block" },
+		bullet_list: { type: basicSchema.BulletList, content: "list_item+", group: "block" },
+		horizontal_rule: { type: basicSchema.HorizontalRule, group: "block" },
+		heading: { type: basicSchema.Heading, content: "inline<_>*", group: "block" },
+		code_block: { type: basicSchema.CodeBlock, content: "text*", group: "block" },
+
+		list_item: { type: basicSchema.ListItem, content: "paragraph block*" },
+
+		table: { type: tableSchema.Table, content: "table_row[columns=.columns]+", group: "block" },
+		table_row: { type: tableSchema.TableRow, content: "table_cell{.columns}" },
+		table_cell: { type: tableSchema.TableCell, content: "inline<_>*" },
+
 		text: {type: basicSchema.Text, group: "inline"},
 		hard_break: {type: basicSchema.HardBreak, group: "inline"}
 	},
@@ -50,6 +65,7 @@ exports.init = function(config) {
 	}
 
 	let pm = new ProseMirror(opts);
+
 	let menu = buildMenuItems(pm.schema);
 	// keep full menu but remove selectParentNodeItem menu
 	var fullMenu = menu.fullMenu.map(function(arr) {
