1cd43f3eea5fb5e185c1ef2380605b0d3caeda63
Jérémy Lal <kapouer@melix.org>
2017-03-22 11:50:12 +0100

    Focus fixing

--- src/focus-plugin.js
+++ src/focus-plugin.js
@@ -29,16 +29,21 @@ Handler.prototype.click = function(view, pos, e) {
 Handler.prototype.action = function(view, state) {
 	if (this.main.dragging) return;
 	var sel = view.state.tr.selection;
-	if (!sel.empty) return;
-	if (sel.eq(state.tr.selection)) return;
-	this.focus(view, sel.$to);
+	var rpos;
+	if (sel.node) {
+		rpos = sel.$from;
+	} else if (sel.empty) {
+		rpos = sel.$to;
+	}
+	if (rpos == null) return;
+	this.focus(view, rpos);
 };
 
 function focusRoot(view, pos, node, focus) {
+	if (node.type.spec.inline) return; // if node is a Mark
 	var attrs = Object.assign({}, node.attrs);
 	if (focus) attrs.block_focused = true;
 	else delete attrs.block_focused;
-
 	var tr = view.state.tr.setNodeType(pos, null, attrs);
 	tr.addToHistory = false;
 
@@ -46,11 +51,16 @@ function focusRoot(view, pos, node, focus) {
 }
 
 Handler.prototype.focus = function(view, $pos) {
-	var parents = this.main.parents($pos);
-	var root = parents.root;
+	// do not unfocus if view or its document has lost focus
+	if (!view.hasFocus()) {
+		return;
+	}
+	var parents = this.main.parents($pos, true);
+	var root = parents.length && parents[0].root;
 	var pos = root && root.level && root.rpos.before(root.level);
 	var dom = root && this.main.posToDOM(pos);
 	var existing = view.dom.querySelectorAll('[block-focused]');
+
 	var blurs = [];
 	// reverse on purpose here
 	for (var i = existing.length - 1; i >= 0; i--) {
@@ -64,9 +74,10 @@ Handler.prototype.focus = function(view, $pos) {
 
 	if (root && !root.node.attrs.block_focused) {
 		focusRoot(view, pos, root.node, true);
-		while (parents = this.main.parents(root.rpos)) {
-			if (!parents.root || parents.root.node == root.node) break;
-			root = parents.root;
+		var parent;
+		for (var i=1; i < parents.length; i++) {
+			parent = parents[i];
+			root = parent.root;
 			focusRoot(view, root.rpos.before(root.level), root.node, true);
 		}
 	}
