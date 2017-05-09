afd794f222fdf6d1c9575966a697d32711f669c8
Jérémy Lal <kapouer@melix.org>
2017-05-09 18:20:29 +0200

    Let's see if we can just adoptNode instead of clone here

--- src/viewer.js
+++ src/viewer.js
@@ -100,7 +100,7 @@ Viewer.prototype.merge = function(block, dom) {
 		if (!node) return;
 		var content = contents[name];
 		if (!content) return;
-		node.appendChild(content.cloneNode(true));
+		node.appendChild(node.ownerDocument.adoptNode(content));
 	});
 };
 
