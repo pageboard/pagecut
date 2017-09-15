6c7eeb15fc4f1c725c82474815bbc882bdb9aa43
Jérémy Lal <kapouer@melix.org>
2017-09-15 10:44:00 +0200

    Fix domSelector call that prevents correct parsing

--- src/specs.js
+++ src/specs.js
@@ -187,7 +187,7 @@ function createRootSpec(view, elt, obj) {
 	if (elt.tag) {
 		parseRule.tag = elt.tag;
 	} else if (elt.inplace) {
-		parseRule.tag = domSelector(obj.dom.nodeName, {class: obj.className});
+		parseRule.tag = domSelector(obj.dom.nodeName, {class: obj.dom.className});
 	} else {
 		parseRule.tag = `[block-type="${elt.name}"]`;
 	}
