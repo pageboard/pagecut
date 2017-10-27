c7a5d641f6f2fe18184bff8a4816e158fa0fadbb
Jérémy Lal <kapouer@melix.org>
2017-10-27 16:13:47 +0200

    Always restore position after call to update

--- src/specs.js
+++ src/specs.js
@@ -414,8 +414,10 @@ RootNodeView.prototype.update = function(node, decorations) {
 	if (oldBlock && this.dom.update) {
 		// tell custom elements the editor updates this dom node
 		setTimeout(function() {
+			var x = window.scrollX;
+			var y = window.scrollY;
 			this.dom.update();
-			this.view.dispatch(this.view.state.tr.scrollIntoView());
+			window.scrollTo(x, y);
 		}.bind(this), 30);
 	}
 	return true;
