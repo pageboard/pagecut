4417b831c2f2db8e7bf57f9cc48e354729dfd9c6
Jérémy Lal <kapouer@melix.org>
2018-03-21 15:18:35 +0100

    Keep br around... it's not the right way to remove them anyway.

--- src/blocks.js
+++ src/blocks.js
@@ -131,9 +131,6 @@ function nodeToHtml(node) {
 	var html;
 	if (node instanceof Node) {
 		html = "";
-		Array.from(node.querySelectorAll('br')).forEach(function(br) {
-			if (!br.previousSibling || !br.nextSibling) br.remove();
-		});
 		var child;
 		for (var i=0; i < node.childNodes.length; i++) {
 			child = node.childNodes[i];
